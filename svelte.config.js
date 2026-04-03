ea5afc311cd6b6fdec5e9993aa025e6a3b521ebd
David Maat <r1036789@student.thomasmore.be>
2026-04-03 13:28:17 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
             strict: true,
         }),
         paths: {
-            base: process.env.NODE_ENV === "production" ? "/Doelstellingen" : "",
+            base: process.env.NODE_ENV === "production" ? "/" : "",
         },
         alias: {
             $content: "src/content",
